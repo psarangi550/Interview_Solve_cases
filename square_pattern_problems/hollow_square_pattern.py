d853a3c9e51672cb5855eaf4c00e6072e35e4613
psarangi550 <psarangi50@gmail.com>
2022-07-17 21:04:07 +0530

    all weekend commit

--- square_pattern_problems/hollow_square_pattern.py
+++ square_pattern_problems/hollow_square_pattern.py
@@ -62,19 +62,54 @@
 
 # hollow triangle pattern
 
-n = int(input("Enter the number of rows"))
+# n = int(input("Enter the number of rows"))
 
-for i in range(n):
-    for j in range(i, n):
-        print(" ", end=" ")
-    for j in range(i):
-        if j == 0 or i == n - 1:
-            print("*", end=" ")
-        else:
-            print(" ", end=" ")
-    for j in range(i + 1):
-        if i == n - 1 or i == j:
-            print("*", end=" ")
-        else:
-            print(" ", end=" ")
-    print()
+# for i in range(n):
+#     for j in range(i, n):
+#         print(" ", end=" ")
+#     for j in range(i):
+#         if j == 0 or i == n - 1:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     for j in range(i + 1):
+#         if i == n - 1 or i == j:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     print()
+
+
+# n = int(input("Enter the number of rows"))
+
+# for i in range(n - 1):
+#     for j in range(i, n):
+#         print(" ", end=" ")
+#     for j in range(i):
+#         if j == 0 or i == n - 1:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     for j in range(i + 1):
+#         if i == n - 1 or i == j:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     print()
+
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(" ", end=" ")
+#     for j in range(i, n - 1):
+#         if i == j:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     for j in range(i, n):
+#         if (j == 0 and i > 0) or j == n - 1:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     print()
+
+# alternate ways
