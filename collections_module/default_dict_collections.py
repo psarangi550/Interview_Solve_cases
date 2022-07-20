7fdeb8d81aeb0027080d7aa2a1ead5a15f1a1021
psarangi550 <psarangi50@gmail.com>
2022-07-20 05:44:31 +0530

    All Commits interview

--- collections_module/default_dict_collections.py
+++ collections_module/default_dict_collections.py
@@ -2,9 +2,16 @@ from collections import defaultdict
 
 str1="aaabbbcccddddd"
 
-d=defaultdict(int)
+# d=defaultdict(int)
 
+# for s in str1:
+#     d[s]+=1
+
+d=dict()
 for s in str1:
-    d[s]+=1
+    if s not in d:
+        d[s]=1
+    else:
+        d[s]+=1
 
 print(d)
